6fb003cb2b79cd1a4c5c1209421848bf40f86640
Vincent Hong <vinlegend1@gmail.com>
2020-10-12 14:12:18 +0800

    made User entity and resolver, stopped at 1:23:21

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
+
+// stopped at 17:55
+@ObjectType()
+@Entity()
+export class User {
+
+  @Field(() => Int) // @Field for a graphql field for Post
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: 'date', onUpdate: () => new Date() }) // special hook that creates new date
+  updatedAt = new Date();
+
+  @Field() // if you delete this, you hide the field but still in database
+  @Property({ type: 'text', unique: true }) // if I remove @Property(), it's not a database column, it's just a field of the class
+  username!: string;
+
+  @Property({ type: 'text' }) // if I remove @Property(), it's not a database column, it's just a field of the class
+  password!: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import express from 'express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -17,7 +18,7 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
         context: () => ({ em: orm.em }) // kinda like react context... something accessible by all resolvers
