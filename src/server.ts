a89a554230b9bf6694fadeb1bf63f20b8cd437ea
Vincent Hong <vinlegend1@gmail.com>
2020-11-10 14:33:23 +0800

    stopped at 6:23:08

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const main = async () => {
     type: "postgres",
     database: "lireddit2",
     username: "postgres",
-    password: "postgres",
+    password: "2004",
     logging: true,
     synchronize: true,
     entities: [Post, User],
